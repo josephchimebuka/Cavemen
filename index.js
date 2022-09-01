ca007cb91f61bcc61930b7b611aa6184e93c119b
josephchimebuka <chimebujoseph@gmail.com>
2022-09-01 17:43:06 +0100

    added jquery function

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const benjiPage =  document.getElementById("BenjiSection");
-const okoriePage = document.getElementById("OkorieSection");
+// const benjiPage =  document.getElementById("BenjiSection");
+// const okoriePage = document.getElementById("OkorieSection");
 
-function changingPages(){
-if(okoriePage.style.display === "none"){
-    benjiPage.style.display === "block"
-}else if( benjiPage.style.display === "none"){
-    okoriePage.style.display === "block"
-}
-}
+// function changingPages(){
+// if(okoriePage.style.display === "none"){
+//     benjiPage.style.display === "block"
+// }else if( benjiPage.style.display === "none"){
+//     okoriePage.style.display === "block"
+// }
+// }
 
-changingPages();
+// changingPages();
+
+
+function swapImage(){
+    $("#BenjiSection").fadeOut();
+    $("#OkorieSection").fadeIn();
+    setTimeout( function(){
+        {
+            $("#OkorieSection").fadeOut();
+            $("#BenjiSection").fadeIn();
+        }
+    }, 2000);
+    setTimeout(function(){
+        swapImage();
+    }, 4000)
+}
+$(document).ready(function() {
+    swapImage();
+});
