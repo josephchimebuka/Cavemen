b2e0cc58fe48f316a227806166a6752c2344cf9b
josephchimebuka <chimebujoseph@gmail.com>
2022-09-01 13:38:24 +0100

    javascript updates

---
+++ index.js
@@ -0,0 +1,9 @@
+const benjiPage =  document.getElementById('BenjiSection');
+const okoriePage = document.getElementById('OkorieSection');
+
+function changingPages(){
+    benjiPage.style.display = none;
+    okoriePage.style.display = block;
+}
+
+changingPages();
