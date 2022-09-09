821fc59ac095547c408d4ced82b4c7436eb2c4c6
josephchimebuka <chimebujoseph@gmail.com>
2022-09-09 15:18:24 +0100

    Text animation working

--- index.js
+++ index.js
@@ -12,23 +12,33 @@
 // changingPages();
 
 
-function swapImage(){
-    $("#BenjiSection").show(10000);
-    $("#BenjiSection").fadeOut(6000);
-    $("#OkorieSection").fadeIn(6000);
-    $("#OkorieSection").show(10000);
-    $("#BenjiSection").fadeOut(6000);
-    setTimeout( function(){
-        {
-            $("#OkorieSection").show(10000);
-            $("#OkorieSection").fadeOut(6000);
-            $("#BenjiSection").fadeIn(6000);
-        }
-    }, 6000);
-    setTimeout(function(){
-        swapImage();
-    }, 8000)
-}
-$(document).ready(function() {
-    swapImage();
-});
+// function swapImage(){
+//     $("#BenjiSection").show(10000);
+//     $("#BenjiSection").fadeOut(6000);
+//     $("#OkorieSection").fadeIn(6000);
+//     $("#OkorieSection").show(10000);
+//     $("#BenjiSection").fadeOut(6000);
+//     setTimeout( function(){
+//         {
+//             $("#OkorieSection").show(10000);
+//             $("#OkorieSection").fadeOut(6000);
+//             $("#BenjiSection").fadeIn(6000);
+//         }
+//     }, 6000);
+//     setTimeout(function(){
+//         swapImage();
+//     }, 8000)
+// }
+// $(document).ready(function() {
+//     swapImage();
+// });
+
+
+let t1 =  gsap.timeline();
+t1.from("p",{opacity:0, duration:1, y:40})
+
+t1.from(".benName span",1,{opacity:0,x:-30, y:-20, scale: 2,
+  opacity: 0, duration:2,  ease: "back.out(2)",
+stagger: {
+  amount: 0.7
+} })
