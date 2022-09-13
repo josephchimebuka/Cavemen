67b0280adb458ed8bbd4cc93e775eaeffecef532
josephchimebuka <chimebujoseph@gmail.com>
2022-09-13 17:46:57 +0100

    Added text for nav elements

--- index.js
+++ index.js
@@ -33,31 +33,37 @@
 //     swapImage();
 // });
 
+function textAnimation(){
 
-let t1 =  gsap.timeline();
-t1.from("p",{opacity:0, duration:1, y:40})
-
-t1.from(".benName span",1,{opacity:0,x:-30, y:-20, scale: 2,
-  opacity: 0, duration:2,  ease: "back.out(2)",
-stagger: {
-  amount: 0.7
-} })
+  let t1 =  gsap.timeline();
+  t1.from("p",{opacity:0, duration:1, y:40})
+  
+  t1.from(".benName span",1,{opacity:0,x:-30, y:-20, scale: 2,
+    opacity: 0, duration:2,  ease: "back.out(2)",
+  stagger: {
+    amount: 0.7
+  } })
+}
+textAnimation();
 
 let images =  gsap.utils.toArray(".bg"),
 t2 = gsap.timeline({repeat:-1}),
-fadeDuration = 1.5,
-stayDuration = 3;
+fadeDuration = 2,
+stayDuration = 8;
 
-gsap.set(images[0], {autoAlpha:1})
+gsap.set(images[0], {autoAlpha: 1});
 
-t2.to(images[1], {delay:stayDuration, autoAlpha:1,
-  duration: fadeDuration,stagger: fadeDuration + stayDuration})
+t2.from(images[0], {delay:stayDuration, autoAlpha:0,
+  duration: fadeDuration,stagger: 2}),
+ 
+  t2.to(images[1], {delay:stayDuration, autoAlpha:1,
+    duration: fadeDuration,stagger: 2})
+  
+  .to(images[1], {autoAlpha: 0, duration:fadeDuration, stagger: stayDuration + fadeDuration}, stayDuration + fadeDuration)
+  .set(images[0], {autoAlpha: 1, duration: fadeDuration})
 
-  .to(images[0], {autoAlpha: 0, duration:0.01, stagger: stayDuration + fadeDuration}, stayDuration + fadeDuration)
-  .set(images[0], {autoAlpha: 1})
 
-  .to(images[1], {autoAlpha: 0, duration: fadeDuration}, "+=" + stayDuration);
-
+  .to(images[images.length+1], {autoAlpha: 1, duration: fadeDuration}, "+=" + stayDuration);
 
 
 
